7a931d46e4658e1eb438a6aadef3596fb1f2a28c
MuAyman <s-mo_ayman@zewailcity.edu.eg>
2024-07-22 07:59:43 +0300

    deleting gitignore

---
+++ build/CMakeFiles/CPPsub_BlocksRearrange_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPsub_BlocksRearrange_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CPPsub_BlocksRearrange_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
